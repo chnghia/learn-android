e0f5327145c2cf66fcf8ce409c1cadb7b26dad01
Nghia Chung <chnghia@gmail.com>
2014-07-10 14:42:16 +0700

    Chapter 8: Fragments

--- app/src/main/java/com/marakana/yamba/StatusActivity.java
+++ app/src/main/java/com/marakana/yamba/StatusActivity.java
@@ -1,36 +1,32 @@
 package com.marakana.yamba;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 
-
-public class StatusActivity extends ActionBarActivity {
-
+public class StatusActivity extends Activity {
+    private static final String TAG = "StatusActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_status);
+        Log.d(TAG, "onCreate"); //
+//        setContentView(R.layout.new_activity_status);
+        // Check if this activity was created before
+        if (savedInstanceState == null) { //
+            // Create a fragment
+            StatusFragment fragment = new StatusFragment(); //
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, fragment,
+                            fragment.getClass().getSimpleName())
+                    .commit(); //
+        }
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.status, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
