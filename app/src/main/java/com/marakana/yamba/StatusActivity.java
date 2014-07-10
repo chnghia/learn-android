90e3296e0c5042760be8f1df95158a01213c6bf2
Nghia Chung <chnghia@gmail.com>
2014-07-10 16:46:28 +0700

    Chapter 10: Services

--- app/src/main/java/com/marakana/yamba/StatusActivity.java
+++ app/src/main/java/com/marakana/yamba/StatusActivity.java
@@ -1,9 +1,11 @@
 package com.marakana.yamba;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class StatusActivity extends Activity {
     private static final String TAG = "StatusActivity";
@@ -27,6 +29,20 @@ public class StatusActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.status, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemServiceStart:
+                startService(new Intent(this, RefreshService.class)); //
+                break;
+            case R.id.itemPrefs:
+                startActivity(new Intent(this, SettingsActivity.class));
+                break;
+            default:
+                return false;
+        }
         return true;
     }
 }
